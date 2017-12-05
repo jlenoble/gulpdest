83b8fcaa5cf8bdd02e793853479a3820f1979cae
Jason Lenoble <jason.lenoble@gmail.com>
2017-12-05 02:57:18 +0100

    Don't write in build dir
    Files are watched and tests enter an infinite loop.

--- test/examples.test.js
+++ test/examples.test.js
@@ -4,17 +4,18 @@ import gulp from 'gulp';
 import path from 'path';
 import Muter, {muted} from 'muter';
 import equalStreamContents from 'equal-stream-contents';
+import {tmpDir} from 'cleanup-wrapper';
 
 describe('Testing README.md examples', function () {
-  it(`Usage example`, function () {
-    const dest = new GulpDest('build');
+  it(`Usage example`, tmpDir('build4', function () {
+    const dest = new GulpDest('build4');
     const glob = 'src/**/*.js';
-    const stream = gulp.src(glob);
+    const stream = gulp.src(glob, {base: process.cwd()});
 
     const gg = dest.dest(stream, {glob}); // Writes stream to 'build' and
     // returns a GulpGlob object
 
-    return gg.toPromise().then(globs => {
+    return gg.toPromise().then(() => {
       const muter = Muter(console, 'log'); // eslint-disable-line new-cap
 
       return Promise.all([
@@ -22,12 +23,12 @@ describe('Testing README.md examples', function () {
           const cwd = process.cwd();
           return gg.list().then(list => {
             expect(list).to.eql([
-              path.join(cwd, 'build', 'src/gulpdest.js'),
+              path.join(cwd, 'build4', 'src/gulpdest.js'),
             ]);
           });
         })(),
-        equalStreamContents(gg.src(), gulp.src(path.join('build', glob))),
+        equalStreamContents(gg.src(), gulp.src(path.join('build4', glob))),
       ]);
     });
-  });
+  }));
 });
